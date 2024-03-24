9f2c53307a2be323503494c396d43df8870df267
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2024-03-24 15:44:55 +0000

    Add tiling task that doesn't leak memory

--- workflows/tiles_parquet.py
+++ workflows/tiles_parquet.py
@@ -4,13 +4,16 @@ usage:
     python -m workflows.geotiff_parquet
 """
 
+import gc
 import os
+from multiprocessing import Pool
 from pathlib import Path
 
 import luigi
 import luigi.contrib.gcs
 import pandas as pd
 import torch
+import tqdm
 
 from geolifeclef.loaders.GLC23Datasets import PatchesDataset
 from geolifeclef.loaders.GLC23PatchesProviders import (
@@ -26,9 +29,30 @@ class TilingTask(luigi.Task):
     meta_path = luigi.Parameter()
     output_path = luigi.Parameter()
     batch_size = luigi.IntParameter(default=100)
+    num_workers = luigi.IntParameter(default=4)
 
     def output(self):
         return luigi.LocalTarget((Path(self.output_path) / "_SUCCESS").as_posix())
+
+    def write_batch(self, band_names, x, target, item, path):
+        # n x k x 128 x 128 tensor
+        x = x.numpy()
+        target = target.numpy()
+        rows = []
+        for i in range(x.shape[0]):
+            zipped = list(zip(band_names, x[i]))
+            row = {k: v.reshape(-1).astype(float).tolist() for k, v in zipped}
+            row.update({k: v[i].item() for k, v in item.items()})
+            row.update({"target": int(target[i])})
+            rows.append(row)
+        df = pd.DataFrame(rows)
+        path.parent.mkdir(parents=True, exist_ok=True)
+        df.to_parquet(path, index=False)
+        del df, rows, x, target, item
+        gc.collect()
+
+    def mapfn(self, args):
+        self.write_batch(*args)
 
     def run(self):
         input_path = Path(self.input_path)
@@ -59,33 +83,55 @@ class TilingTask(luigi.Task):
             occurrences=(
                 meta_path / "PresenceOnlyOccurrences/GLC24-PO-metadata-train.csv"
             ).as_posix(),
-            providers=[p_bioclim, p_hfp_s, p_rgb],
+            providers=[
+                p_bioclim,
+                p_hfp_s,
+                p_rgb,
+            ],
         )
 
         dataloader = torch.utils.data.DataLoader(
-            dataset, batch_size=self.batch_size, shuffle=False, drop_last=False
+            dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            drop_last=False,
         )
+        # for i, (x, target, item) in tqdm.tqdm(
+        #     enumerate(dataloader), total=len(dataloader)
+        # ):
+        #     self.write_batch(
+        #         dataset.provider.bands_names,
+        #         x,
+        #         target,
+        #         item,
+        #         Path(self.output_path)
+        #         / f"{i//self.batch_size:04d}"
+        #         / f"{i:06d}.parquet",
+        #     )
 
         # write the batch to parquet in many small fragments
         # we put them into subfolders to avoid too many files in a single folder
-        for i, (x, target, item) in enumerate(dataloader):
-            if i > 10:
-                break
-            # n x k x 128 x 128 tensor
-            x = x.numpy()
-            target = target.numpy()
-            rows = []
-            for j in range(x.shape[0]):
-                zipped = list(zip(dataset.provider.bands_names, x[i]))
-                row = {k: v[j].astype(float).tolist() for k, v in zipped}
-                row.update({k: v[j].item() for k, v in item.items()})
-                row.update({"target": int(target[j])})
-                rows.append(row)
-            df = pd.DataFrame(rows)
-            subfolder = Path(self.output_path) / f"{i%self.batch_size:04d}"
-            subfolder.mkdir(parents=True, exist_ok=True)
-            path = subfolder / f"{i:06d}.parquet"
-            df.to_parquet(path, index=False)
+        # parallelize writing batches
+
+        with Pool(self.num_workers) as p:
+            for _ in p.imap(
+                self.mapfn,
+                (
+                    (
+                        dataset.provider.bands_names,
+                        x,
+                        target,
+                        item,
+                        Path(self.output_path)
+                        / f"{i//self.batch_size:04d}"
+                        / f"{i:06d}.parquet",
+                    )
+                    for i, (x, target, item) in tqdm.tqdm(
+                        enumerate(dataloader), total=len(dataloader)
+                    )
+                ),
+            ):
+                pass
 
         with self.output().open("w") as f:
             f.write("")
@@ -101,5 +147,5 @@ if __name__ == "__main__":
             ),
         ],
         scheduler_host="services.us-central1-a.c.dsgt-clef-2024.internal",
-        workers=os.cpu_count(),
+        workers=1,
     )
