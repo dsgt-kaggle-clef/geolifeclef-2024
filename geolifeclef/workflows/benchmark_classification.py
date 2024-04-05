3da01e617baf5d367120e3d730a3da219c101bb7
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2024-04-05 15:25:36 -0700

    Add benchmarking for gpu vs cpu

--- geolifeclef/workflows/benchmark_classification.py
+++ geolifeclef/workflows/benchmark_classification.py
@@ -1,8 +1,12 @@
 from argparse import ArgumentParser
 
 import luigi
+import torch
 from pyspark.ml.classification import RandomForestClassifier
+from pyspark.sql import functions as F
 from xgboost.spark import SparkXGBClassifier
+
+from geolifeclef.utils import spark_resource
 
 from .logistic import BaseFitModel, FitLogisticModel
 from .utils import RsyncGCSFiles
@@ -19,10 +23,14 @@ class FitRandomForestModel(BaseFitModel):
 
 class FitXGBoostModel(BaseFitModel):
     device = luigi.Parameter(default="cpu")
+    num_workers = luigi.IntParameter(default=2)
 
     def _classifier(self, featuresCol: str, labelCol: str):
         return SparkXGBClassifier(
-            features_col=featuresCol, label_col=labelCol, device=self.device
+            features_col=featuresCol,
+            label_col=labelCol,
+            device=self.device,
+            num_workers=self.num_workers,
         )
 
 
@@ -36,31 +44,82 @@ class BenchmarkClassificationWorkflow(luigi.Task):
             dst_path=f"{self.local_root}/processed/metadata_clean/v1",
         )
 
-        for k in [3, 20, 100]:
+        for k in [3, 20, 100, 200]:
             yield [
                 # these runs are meant to validate that the pipeline works as expected before expensive runs
-                FitLogisticModel(
-                    k=k,
-                    shuffle_partitions=32,
-                    label="speciesSubsetId",
-                    input_path=f"{self.local_root}/processed/metadata_clean/v1",
-                    output_path=f"{self.local_root}/models/benchmark_classification/v2/model=logistic/k={k}",
+                *(
+                    [
+                        # non-linear scaling behavior for some reason, this
+                        # is terrible as a baseline because it literally takes forever
+                        FitLogisticModel(
+                            k=k,
+                            shuffle_partitions=32,
+                            label="speciesSubsetId",
+                            input_path=f"{self.local_root}/processed/metadata_clean/v1",
+                            output_path=f"{self.local_root}/models/benchmark_classification/v2/model=logistic/k={k}",
+                        ),
+                        # unfortunately this only supports up to 100 classes, but
+                        # otherwise the performance is great
+                        FitRandomForestModel(
+                            k=k,
+                            shuffle_partitions=32,
+                            label="speciesSubsetId",
+                            input_path=f"{self.local_root}/processed/metadata_clean/v1",
+                            output_path=f"{self.local_root}/models/benchmark_classification/v2/model=random_forest/k={k}",
+                        ),
+                    ]
+                    if k <= 100
+                    else []
                 ),
-                FitRandomForestModel(
-                    k=k,
-                    shuffle_partitions=32,
-                    label="speciesSubsetId",
-                    input_path=f"{self.local_root}/processed/metadata_clean/v1",
-                    output_path=f"{self.local_root}/models/benchmark_classification/v2/model=random_forest/k={k}",
-                ),
-                FitXGBoostModel(
-                    k=k,
-                    shuffle_partitions=32,
-                    label="speciesSubsetId",
-                    input_path=f"{self.local_root}/processed/metadata_clean/v1",
-                    output_path=f"{self.local_root}/models/benchmark_classification/v2/model=xgboost/k={k}",
-                ),
+                # multiple workers for xgboost because a single worker is deadfully slow
+                *[
+                    *[
+                        FitXGBoostModel(
+                            k=k,
+                            shuffle_partitions=32,
+                            num_workers=num_workers,
+                            label="speciesSubsetId",
+                            input_path=f"{self.local_root}/processed/metadata_clean/v1",
+                            output_path=f"{self.local_root}/models/benchmark_classification/v2/model=xgboost_n{num_workers}/k={k}",
+                        )
+                        for num_workers in [2, 8, 16]
+                    ],
+                    *(
+                        [
+                            FitXGBoostModel(
+                                k=k,
+                                shuffle_partitions=32,
+                                num_workers=num_workers,
+                                device="gpu",
+                                label="speciesSubsetId",
+                                input_path=f"{self.local_root}/processed/metadata_clean/v1",
+                                output_path=f"{self.local_root}/models/benchmark_classification/v2/model=xgboost_gpu_n{num_workers}/k={k}",
+                            )
+                            # we can only have one worker per gpu
+                            for num_workers in [1]
+                        ]
+                        if torch.cuda.is_available()
+                        else []
+                    ),
+                ],
             ]
+
+        # now print the results
+        with spark_resource() as spark:
+            df = (
+                spark.read.json(
+                    f"{self.local_root}/models/benchmark_classification/v2/*/*/perf"
+                )
+                .withColumn("file", F.input_file_name())
+                .select(
+                    F.regexp_extract("file", r"model=(\w+)", 1).alias("model"),
+                    F.regexp_extract("file", r"k=(\d+)", 1).cast("integer").alias("k"),
+                    F.round(F.col("avg_metrics")[0], 2).alias("f1_avg"),
+                    F.round(F.col("std_metrics")[0], 2).alias("f1_std"),
+                    F.round("train_time", 2).alias("train_time"),
+                )
+            ).orderBy("model", "k")
+            df.show(n=100)
 
 
 if __name__ == "__main__":
